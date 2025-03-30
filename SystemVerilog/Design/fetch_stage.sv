3d5b34c23e201b8c9b87c405046481226b2e6204
xlK02 <122417137+xlK02@users.noreply.github.com>
2025-03-30 22:44:07 +0200

    MUX of branch & pc_src

--- SystemVerilog/Design/fetch_stage.sv
+++ SystemVerilog/Design/fetch_stage.sv
@@ -5,10 +5,12 @@ module fetch_stage(
     input clk,
     input reset_n,
     output logic [31:0] address,
-    input [31:0] data
+    input [31:0] data,
+    input pc_src
 );
 
     logic [31:0] pc_next, pc_reg;
+    logic [31:0] branch_offset;
     
     
     always_ff @(posedge clk) begin
@@ -22,10 +24,24 @@ module fetch_stage(
         
         
     always_comb begin
-        pc_next = pc_reg + 4;      
+        case (data[6:0])
+            7'b1100011: begin
+                branch_offset = {{19{data[31]}}, data[31], data[7], data[30:25], data[11:8], 1'b0}; //Btype
+            end
+            7'b1101111: begin
+                branch_offset = {{11{data[31]}}, data[31], data[19:12], data[20], data[30:21], 1'b0}; //Jal
+            end
+            7'b1100111: begin
+                branch_offset = {{20{data[31]}}, data[31:20]}; //Jalr
+            end
+            default: begin
+                branch_offset = 0;
+            end
+        endcase
     end
     
     
     assign address = pc_reg;
+    assign pc_next = pc_src? pc_reg + branch_offset : pc_reg + 4;
     
 endmodule
