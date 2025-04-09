ffd9f350b44ff5356c55f25d3b2ffe4dc01f2dac
Liiiiinn <sqhxytrv@qq.com>
2025-04-09 17:14:17 +0200

    add part of RV32I MAPPING

--- SystemVerilog/Design/control.sv
+++ SystemVerilog/Design/control.sv
@@ -8,17 +8,9 @@ module control(
     input reset_n,
     input instruction_type instruction, 
     output control_type control
-);
-
-    localparam logic [16:0] ADD_INSTRUCTION = {7'b0000000, 3'b000, 7'b0110011};
-    localparam logic [16:0] SUB_INSTRUCTION = {7'b0100000, 3'b000, 7'b0110011};
-    localparam logic [9:0] ADDI_INSTRUCTION = {3'b000, 7'b0010011};
-    localparam logic [9:0] LW_INSTRUCTION = {3'b010, 7'b0000011};
-    localparam logic [9:0] SW_INSTRUCTION = {3'b010, 7'b0100011};
-    localparam logic [9:0] BEQ_INSTRUCTION = {3'b000, 7'b1100011};
+);  
     
-    
-    always_comb begin //分析insruction的内容，设置control
+    always_comb begin
         control = '0;
         
         case (instruction.opcode)
@@ -77,16 +69,70 @@ module control(
             end
         endcase
 
-        control.alu_op = ALU_ADD;
-        if ({instruction.funct7, instruction.funct3, instruction.opcode} == ADD_INSTRUCTION) begin
-            control.alu_op = ALU_ADD;
-        end 
-        else if ({instruction.funct7, instruction.funct3, instruction.opcode} == SUB_INSTRUCTION) begin
-            control.alu_op = ALU_SUB;
-        end 
-        else if ({instruction.funct3, instruction.opcode} == BEQ_INSTRUCTION) begin
-            control.alu_op = ALU_SUB;
-        end            
+        //RV32I
+        case (instruction.opcode)
+            //R type, SLL, SRL, SRA, ADD, SUB, XOR, OR, AND, SLT, SLTU
+            7'b0110011: begin
+                unique casez ({instruction.funct7[5], instruction.funct3})
+                    4'b0_000: control.alu_op = ALU_ADD;
+                    4'b1_000: control.alu_op = ALU_SUB;
+                    4'b?_001: control.alu_op = ALU_SLL;
+                    4'b?_010: control.alu_op = ALU_SLT;
+                    4'b?_011: control.alu_op = ALU_SLTU;
+                    4'b?_100: control.alu_op = ALU_XOR;
+                    4'b0_101: control.alu_op = ALU_SRL;
+                    4'b1_101: control.alu_op = ALU_SRA;
+                    4'b?_110: control.alu_op = ALU_OR;
+                    4'b?_111: control.alu_op = ALU_AND;
+                endcase
+            end
+
+            //I type, SLLI, SRLI, SRAI, ADDI, XORI, ORI, ANDI, SLTI, SLTIU
+            7'b0010011: begin
+                unique casez ({instruction.funct7[5], instruction.funct3})
+                    4'b?_000: control.alu_op = ALU_ADD;    //addi
+                    4'b?_001: control.alu_op = ALU_SLL;    //slli
+                    4'b?_010: control.alu_op = ALU_SLT;    //slti
+                    4'b?_011: control.alu_op = ALU_SLTU;   //sltiu
+                    4'b?_100: control.alu_op = ALU_XOR;    //xori
+                    4'b0_101: control.alu_op = ALU_SRL;    //srli
+                    4'b1_101: control.alu_op = ALU_SRA;    //srai
+                    4'b?_110: control.alu_op = ALU_OR;     //ori
+                    4'b?_111: control.alu_op = ALU_AND;    //andi
+                endcase
+            end
+
+            //I type, LB, LH, LBU, LHU, LW
+            7'b0000011: begin
+                unique casez (instruction.funct3)
+                    3'b000: control.alu_op = ALU_ADD;   //lb
+                    3'b001: control.alu_op = ALU_ADD;   //lh
+                    3'b010: control.alu_op = ALU_ADD;   //lw
+                    3'b100: control.alu_op = ALU_ADD;   //lbu
+                    3'b101: control.alu_op = ALU_ADD;   //lhu
+                endcase
+            end
+
+            //I type, JALR
+            7'b1100111: control.alu_op = ALU_ADD;
+            //J type, JAL
+            7'b1101111: control.alu_op = ALU_ADD;
+
+            //S type, SB, SH, SW
+            7'b0100011: begin
+                unique casez (instruction.funct3)
+                    3'b000: control.alu_op = ALU_ADD;   //sb
+                    3'b001: control.alu_op = ALU_ADD;   //sh
+                    3'b010: control.alu_op = ALU_ADD;   //sw
+                endcase
+            end
+
+            //B type, BEQ, BNE, BLT, BGE, BLTU, BGEU
+            7'b1100011: control.alu_op = ALU_SUB;
+
+            //TODO: U type
+        endcase
+
     end
     
 endmodule
