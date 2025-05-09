fcce61839cb35ae0ba00cdbf6d6365b3f3ed69c8
Bougasta <fantingyi13@gmail.com>
2025-05-09 12:44:04 +0200

    Update gshare_predictor.sv

--- SystemVerilog/Design/gshare_predictor.sv
+++ SystemVerilog/Design/gshare_predictor.sv
@@ -8,6 +8,7 @@ module gshare_predictor#(
     input logic clk,
     input logic reset_n,
     input logic [31:0] pc,
+    input logic [31:0] branch_offset,
     input logic update,            // 是否更新预测器
     input logic actual_taken,      // 实际是否跳转
     output logic prediction        // 预测结果（跳转 or 不跳转）
@@ -15,12 +16,15 @@ module gshare_predictor#(
 
     logic [GHR_BITS-1:0] ghr,ghr_d;  // Global History Register    
     logic [1:0] bht [0:BHT_SIZE-1];  // Branch History Table   
-    logic [$clog2(BHT_SIZE)-1:0] index, index_d;// BHT index
+    logic [$clog2(BHT_SIZE)-1:0] index;// BHT index
     logic [1:0] counter;  // counter now
     logic [7:0]pc_part;
+    logic [31:0] pc_generating_branch;
 
-    assign pc_part = pc[GHR_BITS+1:2]; //when current EXE is branch,update ==1, the pc is  not the pc generating branch 
-    assign index = pc_part ^ ghr_d; //hash only consider from bit[2]，bit[1:0] is for alignment
+
+    assign pc_generating_branch = pc-branch_offset -4; //when current EXE is branch,update ==1, the pc is  not the pc generating branch 
+    assign pc_part = pc_generating_branch[GHR_BITS+1:2]; //hash bit[1:0] only used for alignment in regular instruction，
+    assign index = pc_part ^ ghr_d; 
     assign counter = bht[index];
     assign prediction = counter[1];
     
@@ -28,10 +32,10 @@ module gshare_predictor#(
         if (!reset_n) begin
             ghr <= '0;
             ghr_d <= '0;
-            index_d <= '0;
+          //  index_d <= '0;
         end else begin
             ghr_d <= ghr;
-            index_d <= index;
+        //    index_d <= index;
             if (update) begin
                 ghr <= {ghr[GHR_BITS-2:0], actual_taken};
  //               index <= (pc[GHR_BITS+1:2]) ^ ghr;
@@ -47,18 +51,18 @@ module gshare_predictor#(
         if (!reset_n) begin
       //     ghr <= '0;
             for (int i = 0; i < BHT_SIZE; i++) begin
-                bht[i] <= 2'b10; // weak taken for test the flush function
+                bht[i] <= 2'b10; // 默认弱不跳转
             end
         end 
         else begin
             // 更新预测器
             if (update) begin
                 // 更新 BHT 饱和计数器
-                case (bht[index_d])
-                    2'b00: bht[index_d] <= actual_taken ? 2'b01 : 2'b00;// strong not taken
-                    2'b01: bht[index_d] <= actual_taken ? 2'b10 : 2'b00;// weak not taken
-                    2'b10: bht[index_d] <= actual_taken ? 2'b11 : 2'b01;// weak taken
-                    2'b11: bht[index_d] <= actual_taken ? 2'b11 : 2'b10;//strong taken
+                case (bht[index])
+                    2'b00: bht[index] <= actual_taken ? 2'b01 : 2'b00;// strong not taken
+                    2'b01: bht[index] <= actual_taken ? 2'b10 : 2'b00;// weak not taken
+                    2'b10: bht[index] <= actual_taken ? 2'b11 : 2'b01;// weak taken
+                    2'b11: bht[index] <= actual_taken ? 2'b11 : 2'b10;//strong taken
                 endcase
 
                 // 更新 GHR（移位寄存器）
