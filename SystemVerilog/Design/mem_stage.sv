5f56c352a28d01090c26c7e1ba06222ecd5d5eb2
Bougasta <fantingyi13@gmail.com>
2025-05-10 12:58:01 +0200

    Update mem_stage.sv

--- SystemVerilog/Design/mem_stage.sv
+++ SystemVerilog/Design/mem_stage.sv
@@ -9,14 +9,16 @@ module mem_stage(
     input [31:0] alu_data_in,
     input [31:0] memory_data_in,
     input control_type control_in,
+    input logic [31:0] pc_in,
     output control_type control_out,
     output logic [31:0] memory_data_out,
+    output logic [31:0] pc_out,
     output logic [31:0] alu_data_out
 );
 
     data_memory inst_mem(
         .clk(clk),        
-        .byte_address(alu_data_out[9:0]), //only use least significant 10 bits
+        .byte_address(alu_data_out[9:0]),
         .write_enable(control_in.mem_write),
         .write_data(memory_data_in),
         .read_data(memory_data_out)
@@ -25,5 +27,6 @@ module mem_stage(
 
     assign alu_data_out = alu_data_in;    
     assign control_out = control_in;
+    assign pc_out = pc_in;
     
 endmodule
