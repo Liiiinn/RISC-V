f79371c8e707d86fd190ae60a979c675312ef594
Liiiiinn <sqhxytrv@qq.com>
2025-06-02 22:19:42 +0200

    add code of test6, 7, 8

--- SystemVerilog/Design/program_memory.sv
+++ SystemVerilog/Design/program_memory.sv
@@ -53,45 +53,74 @@ module program_memory (
         // ram[8] = 32'b0000000_00110_00000_000_01000_0010011; //x8 =6;
         // ram[8] = 32'b0000000_00000_00000_000_00000_0010011; // (end: nop)*/
 
-        //ram[0] = 32'h0020_0093; // addi x1, x0, 2
-        //ram[1] = 32'h0593_4529; // c.li x10, 10; then half of addi x11, x0, 5
-        //ram[2] = 32'h061d_0050; // half of addi x11, x0, 5; then c.addi x12, 7
-        //ram[3] = 32'h0030_0693; // addi x13, x0, 3
-        //ram[4] = 32'h458d_9506; // c.add x10, x1; then c.li x11, 3
-        //ram[5] = 32'h862a_8d0d; // c.sub x10, x11; then c.mv x12, x10
-        //ram[6] = 32'h8909_8e69; // c.and x12, x10; then c.andi x10, 2
-        //ram[7] = 32'h8d35_8e49; // c.or x12, x10; then c.xor x10, x13
-        //ram[8] = 32'h0592_6595; // c.lui x11, 5; then c.slli x11, 4
-        //ram[9] = 32'h0713_8591; // c.srai x11, 4; then half of addi x14, x0, -1
-        //ram[10] = 32'h8311_fff0; // half of addi x14, x0, -1; then c.srli x14, 4
-        //ram[11] = 32'h42d0_c2cc; // c.sw x11, 4(x13), then c.lw x12, 4(x13)
 
-        //ram[12] = 32'h0000_0613;  // addi x12, x0, 0
-        //ram[13] = 32'h0030_0693;  // addi x13, x0, 3
-        //ram[14] = 32'h0000_0713;  // addi x14, x0, 0
-        //ram[15] = 32'h0813_4785;  // c.li x15, 1; then half of addi x16, x0, 1
-        //ram[16] = 32'h0613_0010;  // half of addi x16, x0, 1; then half of addi x12, x12, 1
+        // ram[0] = 32'h0020_0093; // addi x1, x0, 2
+        // ram[1] = 32'h0593_4529; // c.li x10, 10; then half of addi x11, x0, 5
+        // ram[2] = 32'h061d_0050; // half of addi x11, x0, 5; then c.addi x12, 7
+        // ram[3] = 32'h0030_0693; // addi x13, x0, 3
+        // ram[4] = 32'h458d_9506; // c.add x10, x1; then c.li x11, 3
+        // ram[5] = 32'h862a_8d0d; // c.sub x10, x11; then c.mv x12, x10
+        // ram[6] = 32'h8909_8e69; // c.and x12, x10; then c.andi x10, 2
+        // ram[7] = 32'h8d35_8e49; // c.or x12, x10; then c.xor x10, x13
+        // ram[8] = 32'h0592_6595; // c.lui x11, 5; then c.slli x11, 4
+        // ram[9] = 32'h0713_8591; // c.srai x11, 4; then half of addi x14, x0, -1
+        // ram[10] = 32'h8311_fff0; // half of addi x14, x0, -1; then c.srli x14, 4
+        // ram[11] = 32'h42d0_c2cc; // c.sw x11, 4(x13), then c.lw x12, 4(x13)
+        // // test 6
+        // ram[0] = 32'h0000_0613;  // addi x12, x0, 0
+        // ram[1] = 32'h0030_0693;  // addi x13, x0, 3
+        // ram[2] = 32'h0000_0713;  // addi x14, x0, 0
+        // ram[3] = 32'h0813_4785;  // c.li x15, 1; then half of addi x16, x0, 1
+        // ram[4] = 32'h0001_0010;  // half of addi x16, x0, 1; then c.nop
+        // // store_loop
+        // ram[5] = 32'h0016_0613;  // addi x12, x12, 1
+        // ram[6] = 32'h00f7_2023;  // sw x15, 0(x14)
+        // ram[7] = 32'h0107_97b3;  // sll x15, x15, x16
+        // ram[8] = 32'h0047_0713;  // addi x14, x14, 4
+        // ram[9] = 32'hfed6_48e3;  // blt x12, x13, store_loop
 
-        //// store_loop
-        //ram[17] = 32'h2023_0016;  // high 16 bits of addi x12, x12, 1; then low 16 bits of sw x15, 0(x14)
-        //ram[18] = 32'h97b3_00f7;  // high 16 bits of sw x15, 0(x14); then low 16 bits of sll x15, x15, x16  
-        //ram[19] = 32'h0713_0107;  // high 16 bits of sll x15, x15, x16; then low 16 bits of addi x14, x14, 4
-        //ram[20] = 32'h48e3_0047;  // high 16 bits of addi x14, x14, 4; then low 16 bits of blt x12, x13, store_loop
-        //ram[21] = 32'h0613_fed6;  // high 16 bits of blt x12, x13, store_loop; then low 16 bits of addi x12, x0, 0
-        //
-        //ram[22] = 32'h0713_0000;  // high 16 bits of addi x12, x0, 0; then low 16 bits of addi x14, x0, 0
-        //ram[23] = 32'h0413_0000;  // high 16 bits of addi x14, x0, 0; then low 16 bits of addi x8, x0, 0
-        //ram[24] = 32'h8693_0000;  // high 16 bits of addi x8, x0, 0; then low 16 bits of addi x13, x13, -1
-        //ram[25] = 32'h0605_fff6;  // high 16 bits of addi x13, x13, -1; load loop: then c.addi x12, 1
+        // ram[10] = 32'h0000_0613;  // addi x12, x0, 0
+        // ram[11] = 32'h0000_0713;  // addi x14, x0, 0
+        // ram[12] = 32'h0000_0413;  // addi x8, x0, 0
+        // ram[13] = 32'hfff6_8693;  // addi x13, x13, -1
+        // // load_loop
+        // ram[14] = 32'h2783_0605;  // c.addi x12, 1; then half of lw x15, 0(x14)
+        // ram[15] = 32'h0713_0007;  // half of lw x15, 0(x14); then addi x14, x14, 4
+        // ram[16] = 32'h8c3d_0047;  // half of addi x14, x14, 4; then c.xor x8, x15
+        // ram[17] = 32'hfec6_dae3;  // bge x13, x12, load_loop
+        // ram[18] = 32'h0000_0013;  // nop
 
-        ram[26] = 32'h0007_2783;  // lw x15, 0(x14)
-        ram[27] = 32'h0047_0713;  // addi x14, x14, 4
-        ram[28] = 32'hdae3_8c3d;  // c.xor x8, x15; low 16 bits of bge x13, x12, load_loop
-        ram[29] = 32'h0013_fec6;  // high 16 bits of bge x13, x12, load_loop; then low 16 bits of nop
+        // // test 7
+        // ram[0] = 32'h0000_0093; // addi x1, x0, 0
+        // ram[1] = 32'h448d_4415; // c.li x8, 5; then c.li x9 3
+        // ram[2] = 32'h14f9_94a2; // c.add x9, x8; then c.addi x9, -2
+        // ram[3] = 32'h8c89_453d; // c.li x10, 15; then c.sub x9, x10
+        // ram[4] = 32'h8085_0506; // c.slli x10, 1; then c.srli x9, 1
+        // ram[5] = 32'hc388_4781; // c.li x15, 0; then c.sw x10, 0(x15)
+        // ram[6] = 32'h0791_c3c4; // c.sw x9, 4(x15); then c.addi x15, 4
+        // ram[7] = 32'ha603_438c; // c.lw x11, 0(x15); then low half of lw x12, -4(x15)
+        // ram[8] = 32'h86aa_ffc7; // high half of lw x12, -4(x15); then c.mv x13, x10
+        // ram[9] = 32'hc699_8e91; // c.sub x13, x12; then c.beqz x13, l
+        // ram[10] = 32'h0010_8093; // addi x1, x1, 1
+        // ram[11] = 32'h0010_8093; // addi x1, x1, 1
+        // ram[12] = 32'h0010_8093; // addi x1, x1, 1
+        // ram[13] = 32'h1230_8093; // l: addi x1, x1, 0x123
+        // ram[14] = 32'h0000_0013; // nop
+        // ram[15] = 32'h0000_0013; // nop
 
-        //ram[30] = 32'h0013_0000;  // high 16 bits of nop; then low 16 bits of nop  
-        //ram[31] = 32'h0000_0000;  // high 16 bits of nop
-        
+        // test 8
+        ram[0] = 32'h8000_0437; // lui x8, 0x8000
+        ram[1] = 32'h0004_0413; // addi x8, x8, 0
+        ram[2] = 32'hffff_f4b7; // lui x9, 0xffff
+        ram[3] = 32'h0001_44bd; // c.li x9, 15; then c.nop
+        // loop:
+        ram[4] = 32'hfff4_8493; // addi x9, x9, -1
+        ram[5] = 32'h0013_8405; // c.srai x8, 1; then low half of nop
+        ram[6] = 32'hf8fd_0000; // high half of nop; then c.bnez x9, loop
+        ram[7] = 32'h0000_0013; // nop
+        ram[8] = 32'h0000_0013; // nop
+
+
     end
     
     
