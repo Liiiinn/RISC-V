42300a2faf845f17afa635cb0f64c3eb8ee76881
Liiiiinn <sqhxytrv@qq.com>
2025-06-07 00:07:31 +0200

    move P&R out

--- SystemVerilog/P&R/pnr_step/_3_power_setup.tcl
+++
@@ -1,8 +0,0 @@
-clearGlobalNets
-globalNetConnect VDD -type tiehi -inst *
-globalNetConnect VDD -type pgpin -pin VDDC -inst *
-globalNetConnect VDD -type pgpin -pin vdd -inst *
-globalNetConnect GND -type pgpin -pin GNDC -inst *
-globalNetConnect GND -type pgpin -pin gnd -inst *
-globalNetConnect GND -type tielo -inst *
-#global netset done
